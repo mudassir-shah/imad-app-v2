bbffa86d614f8c89e0c8955e4e4c6483f59d58e0
mudassir shah <clickmiemail@gmail.com>
2017-03-11 01:58:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,15 @@
 console.log('Loaded!');
 
-//Move the image
-var img = documment.getElementbyId('https://scontent.xx.fbcdn.net/v/t1.0-9/15109555_1235604089834160_8436133228171742354_n.jpg?oh=3bc0ff7b3a8de4b06936eb3b63e6864a&amp;oe=596B3A18');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 10;
-    img.stle.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+//Counter code
+var button = document.getElementbyId('counter');
+var counter = 0;
+
+button.onclick = function () {
+    
+//Make a request to the counter endpoint
+//Capture the response and store it in a variable
+//Render the variable the correct span
+counter = counter + 1;
+var span = document.getElementbyId('count');
+span.innerHTML = counter.toString();
 };
